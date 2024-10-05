94cbb8bdb971b8419b0dbfcdf4264139250d9414
Alisher Narzulloev <92950485+AlisherNarzulloev@users.noreply.github.com>
2024-10-05 19:27:41 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,17 +37,17 @@ with st.sidebar:
   input_penguins = pd.concat([input_df, X_raw], axis=0)
 
 encode = ['island', 'sex']
-df_penguins = pd.get_dummies (input_penguins, prefix=encode)
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
 
-X = df_penguins [1:]
-input_row = df_penguins [:1]
+X = df_penguins[1:]
+input_row = df_penguins[:1]
 
 target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 
 def target_encode(val):
-  return target_mapper [val]
+  return target_mapper[val]
   
 y = y_raw.apply(target_encode)
 

9a7cdee1439322f80e9a7f6f676602a1475d1850
Alisher Narzulloev <92950485+AlisherNarzulloev@users.noreply.github.com>
2024-10-05 19:34:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensamble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 with st.expander('Data'):
   st.write('**Raw data**')

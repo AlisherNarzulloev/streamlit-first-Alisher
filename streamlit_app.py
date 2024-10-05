6fb0d98843c9431ad0c2a19ed031666f8435e76c
Alisher Narzulloev <92950485+AlisherNarzulloev@users.noreply.github.com>
2024-10-05 19:05:07 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,8 @@ with st.expander('Data visualization'):
 with st.sidebar: 
   st.header('Inout Features')
   island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgerson'))
-  bill_length_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  bill_length_mm = st.slider('Bill depth (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.sidebar('Bill depth (mm)', 13.1, 21.5, 17.2) 
   flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
   gender = st.selectbox('Gender', ('male', 'female'))
